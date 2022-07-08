0c896057de6b13f309b334b74071e728436125b0
Simon Berndtsson <simon.berndtsson@instabox.se>
2022-07-08 18:01:36 +0200

    Init: Set up basic data models and templates

--- beerolympics/beerolympics/urls.py
+++ beerolympics/beerolympics/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from games.views import GamesView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('', GamesView.as_view()),
 ]
--- beerolympics/games/models.py
+++ beerolympics/games/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Game(models.Model):
+    name = models.CharField(max_length=255, blank=False, null=False)
+    rules = models.TextField()
+
+
+class Match(models.Model):
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    team_one = models.ForeignKey('players.Team', on_delete=models.CASCADE, related_name='match_team_one')
+    team_two = models.ForeignKey('players.Team', on_delete=models.CASCADE, related_name='match_team_two')
+
